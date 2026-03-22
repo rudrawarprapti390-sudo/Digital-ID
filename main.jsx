54a9b0446958cb59ed1ec426b8fc44b9e742ed48
rudrawarprapti390-sudo <rudrawarprapti390@gmail.com>
2026-03-22 10:42:29 +0530

    Add files via upload

---
+++ main.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
+import axios from 'axios'
+
+// Set default base URL for axios
+axios.defaults.baseURL = 'http://localhost:5000'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+)
